5179374bd086a02421523f807504eb121b7fc7c0
AxelJaBo <axel.910921@gmail.com>
2024-01-09 05:06:28 -0800

    Tipos para referencias y observadores

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
 type Props = { image: string}
 
 const RandomFox = ({ image }: Props): JSX.Element => {
+    const node = useRef<HTMLImageElement>(null);
     return (
         <img
+            ref={node}
             width="320"
             height="auto"
             src={image}
