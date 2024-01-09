b4d8fd0c821ae878b2279b58ef4b160233b50c3c
AxelJaBo <axel.910921@gmail.com>
2024-01-09 04:04:45 -0800

    State con tipos primitivos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import { RandomFox } from "@/components/RandomFox"
+import { useState } from "react";
 
 // Generates a random number between 1 and 122
 const randomNumber = () => Math.floor(Math.random() * 122) + 1;
 
 export default function Home() {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    `https://randomfox.ca/images/${randomNumber()}.jpg`
+  ]);
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        <RandomFox image={`https://randomfox.ca/images/${randomNumber()}.jpg`}/>
+        {
+          images.map((image, index) => (
+            <div key={index} className="p-4">
+              <RandomFox image={image}/>
+            </div>
+          ))
+        }
       </h1>
     </>
   )
